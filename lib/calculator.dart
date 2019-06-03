deed4268c4fbe81045b48c4cc4844a8910d89df8
Cheryl Bennett <cherylbennett@cheryls-air.home>
2019-06-03 15:33:09 +0100

    start to setup layout of calculator

---
+++ lib/calculator.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class Calculator extends StatefulWidget {
+  @override
+  State createState() => new CalculatorState();
+}
+
+class CalculatorState extends State<Calculator> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Weight Calculator"),
+        ),
+        body: Container(
+            height: double.infinity,
+            width: double.infinity,
+            color: Colors.black,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Flexible(
+                  flex: 3,
+                  child: Container(
+                    color: Colors.black,
+                  ),
+                ),
+                Flexible(
+                  flex: 7,
+                  child: Container(
+                    color: Colors.white,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Flexible(
+                          flex: 1,
+                          child: Container(
+                            color: Colors.black12,
+                          ),
+                        ),
+                        Flexible(
+                          flex: 1,
+                          child: Container(
+                            color: Colors.black26,
+                          ),
+                        ),
+                        Flexible(
+                          flex: 1,
+                          child: Container(
+                            color: Colors.black38,
+                          ),
+                        ),
+                        Flexible(
+                          flex: 1,
+                          child: Container(
+                            color: Colors.black54,
+                          ),
+                        ),
+                        Flexible(
+                          flex: 1,
+                          child: Container(
+                            color: Colors.black12,
+                          ),
+                        ),
+                        Flexible(
+                          flex: 1,
+                          child: Container(
+                            color: Colors.black26,
+                          ),
+                        ),
+                        Flexible(
+                          flex: 1,
+                          child: Container(
+                            color: Colors.black38,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            )));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'home_page.dart';
+import 'calculator.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Weight Calculator App',
       theme: ThemeData.dark(),
-      home: HomePage(),
+      home: Calculator(),
     );
   }
 }
